2047e544ac0d8cea9f328d6ddfae8ba077d94763
convivial-parser-7775 <avelonpang@gmail.com>
2020-10-14 01:29:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,8 +26,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   #planeteer_calls.find { |valid_calls|}
-planeteer_calls.find { |valid_calls| valid_calls == planeteer_calls}
-return valid_calls.match 
+p planeteer_calls.find { |valid_calls| valid_calls == planeteer_calls}
 
 end 
 
